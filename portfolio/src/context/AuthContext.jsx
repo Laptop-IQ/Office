4ab6d29fb2aac14d7c564d5b08be80e9cbb0a180
Laptop-IQ <laptechmind@gmail.com>
2026-06-17 15:51:50 +0530

    link

--- portfolio/src/context/AuthContext.jsx
+++ portfolio/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const API_URL = import.meta.env.VITE_API_URL";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const AuthContext = createContext();
 
